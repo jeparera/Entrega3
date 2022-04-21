1e6bd4fffb76a485fd303ac830f90229ea23ccf9
Jesus Parera <jeparera@ucm.es>
2022-04-21 20:07:39 +0200

    en mi cabeza esto ya funciona

--- 2.py
+++ 2.py
@@ -1,28 +1,33 @@
 from multiprocessing.connection import Listener
-from multiprocessing import Process, Manager, Value, Lock, Condition
+from multiprocessing import Process, Manager, Value, Lock, Condition, Semaphore
 import traceback
 import sys
 import itertools, random
 
 class Jugador():
     def __init__(self):
-        self.mano=[]
         self.cartas = []
-        self.puntos=0
-        
+        self.puntos = 0        
+    
     def robar(self,x,baraja):
         for i in range(x):
-            self.hand.append(baraja[0])
+            self.cartas.append(baraja[0])
             baraja.pop(0)
-            
+    
+    def get_hand(self):
+        return self.cartas
+    
+    def get_points(self):
+        return self.puntos
+    
     def jugar(self,carta,mesa):
-        mesa.append(self.mano[carta])
-        self.mano.pop(carta)
-        
-    def anadir_cartas_puntos(self,mesa): #Esto hay que cambiarlo
+        mesa.append(self.cartas[carta])
+        self.cartas.pop(carta)
+    
+    def anadir_cartas_puntos(self,mesa):
         for i in mesa:
             self.cartas.append(i)
-
+    
     def contar_puntos(self):
         for i in self.cartas:
             if i[0]==1:
@@ -37,7 +42,7 @@ class Jugador():
                 self.puntos +=2
 
 class Game():
-    def __init__(self,manager,baraja):
+    def __init__(self,manager,baraja,semaforo):
         self.running = Value('i', 1)
         self.jugadores=manager.list([Jugador(), Jugador()])
         self.mesa = manager.list([])
@@ -46,30 +51,39 @@ class Game():
         self.mutex = Lock()
         self.esperar = Condition(self.mutex)
         self.turno = Value('i',0)
+    def estado_baraja(self):
+        return(len(self.baraja)!=0)
     def turno_neutral(self):
         self.turno.value = -1
+    
     def cambiar_turno(self):
         self.turno.value = ((self.turno.value+1)%2)
+    
     def turno(self,jugador):
         return(self.turno.value==jugador)
     def ganador(self):
-        total_player1 = self.jugadores[0].contar_puntos
-        total_player2 = self.jugadores[1].contar_puntos
+        self.jugadores[0].contar_puntos
+        self.jugadores[1].contar_puntos
+        total_player1 = self.jugadores[0].puntos()
+        total_player2 = self.jugadores[1].puntos()
         if total_player1<total_player2:
             return("gana el jugador 2 con",total_player2,"puntos")
         elif total_player1>total_player2:
             return("gana el jugador 1 con",total_player1,"puntos")
         else:
             return("empate")
-    def jugar_carta(self,jugador,carta):
+    
+    def jugar_carta(self,jugador,carta,semaforo):
         self.mutex.acquire()
         self.esperar.wait_for(self.turno)
         self.jugadores[jugador].jugar(carta)
         if len(self.mesa)==2:
-            turno_neutral()
+            self.turno_neutral()
+            semaforo.release()
         else:
-            cambiar_turno()
+            self.cambiar_turno()
         self.mutex.release()
+    
     def ganador_turno(self,mesa):
         valor1=mesa[0][0]
         valor2=mesa[1][0]
@@ -93,18 +107,36 @@ class Game():
                 return(2)
             else:
                 return(1)
+    
     def stop(self):
         self.running.value = 0
-    def get_info(self,jugador):
+    
+    def get_info(self):
         info = {
-            'mano player1': self.players.get_pos(),
-            'mano player': self.players[RIGHT_PLAYER].get_pos(),
-            'pos_ball': self.ball[0].get_pos(),
-            'score': list(self.score),
+            'mano player1': self.jugadores[0].get_hand(),
+            'mano player2': self.jugadores[1].get_hand(),
+            'puntos player1': self.jugadores[0].get_points(),
+            'puntos player2': self.jugadores[1].get_points(),
+            'mesa': self.mesa,
             'is_running': self.running.value == 1
             }
         return(info)
-def player(side, conn, game):
+def aux(game,semaforo):
+    try:
+        while game.is_running():
+            semaforo.acquire()
+            ganador = game.ganador_turno(game.mesa)
+            if ganador == 1 and game.estado_baraja():
+                game.jugadores[0].robar(1,game.baraja)
+                game.turno.value=0
+            elif ganador == 2 and game.estado_baraja():
+                game.jugadores[1].robar(1,game.baraja)
+                game.turno.value=1
+            elif not(game.estado_baraja()):
+                game.turno.value=(ganador-1)
+    except:
+        traceback.print_exc()
+def player(side, conn, game,semaforo):
     try:
         conn.send( (side, game.get_info()) )
         while game.is_running():
@@ -112,11 +144,14 @@ def player(side, conn, game):
             while command != "next":
                 command = conn.recv()
                 if command == "0":
-                    game.jugar_carta(side,0,game.mesa)
+                    game.jugar_carta(side,0,game.mesa,semaforo)
                 elif command == "1":
-                    game.jugar_carta(side,1,game.mesa)
+                    game.jugar_carta(side,1,game.mesa,semaforo)
                 elif command == "2":
-                    game.jugar_carta(side,2,game.mesa)
+                    game.jugar_carta(side,2,game.mesa,semaforo)
+                elif command == "vacio":
+                    game.ganador()
+                    game.stop()
                 elif command == "quit":
                     game.stop()
             conn.send(game.get_info())
@@ -128,27 +163,31 @@ def player(side, conn, game):
 
 def main(ip_address):
     manager = Manager()
-    mesa = []
     baraja = manager.list(list(itertools.product([1,2,3,4,5,6,7,10,11,12],['Espadas','Oros','Copas','Bastos'])))
     random.shuffle(baraja)
+    semaforo = Semaphore(0)
     try:
         with Listener((ip_address, 6000),
                       authkey=b'secret password') as listener:
             n_player = 0
             players = [None, None]
-            game = Game(manager)
+            game = Game(manager, baraja)
             while True:
                 print(f"accepting connection {n_player}")
                 conn = listener.accept()
-                players[n_player] = Process(target=Jugador,
-                                            args=(n_player, conn, game))
+                players[n_player] = Process(target=player,
+                                            args=(n_player, conn, game,semaforo))
                 n_player += 1
                 if n_player == 2:
+                    aux = 0
+                    aux = Process(target=aux,
+                                  args=(game,semaforo))
+                    aux.start()
                     players[0].start()
                     players[1].start()
                     n_player = 0
                     players = [None, None]
-                    game = Game(manager,baraja)
+                    game = Game(manager,baraja,semaforo)
 
     except Exception as e:
         traceback.print_exc()
